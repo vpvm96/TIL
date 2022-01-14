e4a41d7636b76a88c51a8231b8152063eb5eda53
vpvm96 <vpvm96@gmail.com>
2022-01-14 18:57:35 +0900

    Problem solving

--- Personal/minMax.js
+++ Personal/minMax.js
@@ -1,28 +1,30 @@
 // 5. 최대 최소찾고 인덱스 값 찾기.
 // ex) 3 1 2 11 10 -> 최대 : 11, 인덱스 3 최소: 1 인덱스 1
 
-const arr = [1, 2, 3, 4, 5];
+const arr = [3, 1, 2, 11, 10];
 
 const max = (arr) => {
-  let maxIndex = arr[0];
+  let max = arr[0];
+  let maxIndex = 0;
   for (let i = 1; i < arr.length; i++) {
-    if (maxIndex < arr[i]) {
-      maxIndex = arr[i];
-      console.log(`최대 :${maxIndex} 인덱스 :${arr[i]}`);
+    if (max < arr[i]) {
+      max = arr[i];
+      maxIndex = i;
     }
   }
-  return maxIndex;
+  return console.log(`최대 :${max} 인덱스 :${maxIndex}`);
 };
 
 const min = (arr) => {
-  let minIndex = arr[0];
+  let min = arr[0];
+  let minIndex = 0;
   for (let i = 1; i < arr.length; i++) {
-    if (minIndex > arr[i]) {
-      minIndex = arr[i];
-      console.log(`최소 :${minIndex} 인덱스 :${arr[i]}`);
+    if (min > arr[i]) {
+      min = arr[i];
+      minIndex = i;
     }
   }
-  return minIndex;
+  return console.log(`최소 :${min} 인덱스 :${minIndex}`);
 };
 
 max(arr);
