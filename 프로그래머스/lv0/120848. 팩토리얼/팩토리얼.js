3f0c024a7225774ffb869286953fc542d7620b85
Leo <86472864+vpvm96@users.noreply.github.com>
2022-12-02 15:41:25 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -6,14 +6,4 @@ function solution(n) {
             return i - 1
         } 
     }
-    
-    /**
-    1 ~ 10 다 곱하면서 n 값이랑 비교하면서 가장 가까운 i 값
-    1
-    1 * 2
-    2 * 3
-    6 * 4
-    */
-    
-    return 
 }
