e4b57e48b1f1986dc1a2a5844feebe64cce787f4
Leo <86472864+vpvm96@users.noreply.github.com>
2022-12-04 23:25:19 +0900

    Update 잘라서 배열로 저장하기.js

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -4,12 +4,10 @@ function solution(my_str, n) {
     
     // for (let i = 0; i < str.length; i++) {
     //     answer.push(str.splice(0, n).join(""))
-    //     console.log(answer)
     // }
     
     while (str.length > 0) {
         answer.push(str.splice(0, n).join(""))
-        console.log(answer)
     }
     
     return answer
