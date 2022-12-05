bb6a246ee12ca1c3105b98b53bee977edd956ad4
Leo <86472864+vpvm96@users.noreply.github.com>
2022-12-05 18:11:59 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    let result = 0
+    
+    for (let i = 0; i < my_string.length; i++) {
+        let thisNum = ""
+        while (!Number.isNaN(Number(my_string[i]))) {
+            thisNum += my_string[i]
+            i++
+        }
+        result += +thisNum
+    }
+    return result
+}
