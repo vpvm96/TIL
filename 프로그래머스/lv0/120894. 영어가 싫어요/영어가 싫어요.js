7a9ab7a02882941fec2d6845552c0e74db504122
Leo <86472864+vpvm96@users.noreply.github.com>
2022-12-08 10:03:10 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    const arr = ["zero","one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine"]
+    
+    for (let i = 0; i < arr.length; i++) {
+        numbers = numbers.split(arr[i]).join(i)
+    }
+    
+    return Number(numbers)
+}
