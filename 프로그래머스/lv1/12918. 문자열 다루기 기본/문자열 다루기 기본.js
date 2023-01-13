d4bfbef5e333e8fd6b0cf42acf53b0859167e188
Leo <86472864+vpvm96@users.noreply.github.com>
2023-01-13 18:11:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,4 @@
 function solution(s) {
-    let result = parseInt(s)
-    
-    if ((s.length === 4 || s.length === 6) && s == result) {
-        result = true
-    } else {
-        result = false
-    }
-    return result
+    var answer = true;
+    return answer;
 }
