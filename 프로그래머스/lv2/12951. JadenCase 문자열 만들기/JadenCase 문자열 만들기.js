5390948db3c2151912035b1df768ac30c6fa2f65
Leo <86472864+vpvm96@users.noreply.github.com>
2023-01-16 14:00:32 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    return s
+        .toLowerCase()
+        .split(" ")
+        .map(v => v
+             .charAt(0)
+             .toUpperCase() + v
+             .substring(1))
+        .join(" ")
+}
