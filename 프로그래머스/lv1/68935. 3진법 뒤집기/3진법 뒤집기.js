d6fc60c50fae8904313a3737ba390cac145671da
Leo <vpvm96@gmail.com>
2023-05-19 22:08:41 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  const answer = [];
+  while (n !== 0) {
+    answer.unshift(n % 3);
+    n = Math.floor(n / 3);
+  }
+  return answer.reduce((acc, v, i) => acc + v * Math.pow(3, i), 0);
+}
